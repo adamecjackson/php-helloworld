fdc3c4ee4d22cf6026a6720a49d5a84ecfbcdff9
Adam <adamjackson@AJ-CAN-MBP.local>
2017-09-17 23:47:56 +0100

    resolved php error

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
  <?php echo '<BR>'; ?>
- <php echo '<p>Here is some new text</p>'; ?>
+ <?php echo '<p>Here is some new text</p>'; ?>
  </body>
 </html>
